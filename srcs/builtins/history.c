b1b9303f661aa1bbe18208fc7d073f223fa70372
Luca BENARD <lubenard@student.42.fr>
2019-10-30 20:28:45 +0100

    Added function to clear hist

--- srcs/builtins/history.c
+++ srcs/builtins/history.c
@@ -6,15 +6,41 @@
 /*   By: lubenard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 17:03:36 by lubenard          #+#    #+#             */
-/*   Updated: 2019/10/30 20:09:18 by lubenard         ###   ########.fr       */
+/*   Updated: 2019/10/30 20:27:57 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <libft.h>
 #include <shell.h>
 #include <utils.h>
 
-int			b_history(t_cmd *cmd, t_shell *shell)
+/*
+** Will clear hist and free linked list
+*/
+
+int		empty_hist(t_shell *shell)
+{
+	t_histo_lst		*history;
+	t_histo_lst		*hist_tmp;
+
+	history = shell->history.lst;
+	while (history)
+	{
+		hist_tmp = history;
+		history = history->prev;
+		ft_strdel(&history->str);
+		free(history);
+		history = NULL;
+	}
+	shell->history.lst = NULL;
+	return (0);
+}
+
+/*
+** Will print last 500 hist var
+*/
+
+int		b_history(t_cmd *cmd, t_shell *shell)
 {
 	t_histo_lst		*history;
 	int				counter;
@@ -29,7 +55,7 @@ int			b_history(t_cmd *cmd, t_shell *shell)
 	}
 	while (history)
 	{
-		ft_printf("  %zu  %s\n", history->index , history->str);
+		ft_printf("  %zu  %s\n", history->index, history->str);
 		history = history->prev;
 	}
 	return (0);
