cba20bbfd32624f931e836b83d46c6fd91357587
Luca BENARD <lubenard@student.42.fr>
2019-11-08 17:36:31 +0100

    Fixed tests and travis

--- tests/test.sh
+++ tests/test.sh
@@ -6,7 +6,7 @@
 #    By: lubenard <lubenard@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/11/03 15:04:01 by lubenard          #+#    #+#              #
-#    Updated: 2019/11/03 15:49:03 by lubenard         ###   ########.fr        #
+#    Updated: 2019/11/08 17:35:53 by lubenard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,4 +21,3 @@ if [[ $output == *"[ERROR]"* ]]; then
 fi
 echo "DONE ! All tests passed !"
 rm log_test
-return 0
