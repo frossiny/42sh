81881803ee3baf35b429567a25c704107669f2b7
Luca BENARD <lubenard@student.42.fr>
2019-11-22 16:45:27 +0100

    Added option feature to jobs

--- srcs/builtins/jobs.c
+++ srcs/builtins/jobs.c
@@ -6,7 +6,7 @@
 /*   By: lubenard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 14:05:36 by lubenard          #+#    #+#             */
-/*   Updated: 2019/11/21 18:44:14 by lubenard         ###   ########.fr       */
+/*   Updated: 2019/11/22 16:32:39 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,13 +52,13 @@ int		*handle_options(t_options *opts)
 			options[0] = 1;
 		else if (!ft_strcmp(opts->opts->opt, "p"))
 			options[1] = 1;
-		opts->opts = opts->opts->next;
+				opts->opts = opts->opts->next;
 	}
 	opts->opts = tmp_opts;
 	return (options);
 }
 
-int		print_jobs(t_shell *shell, int *options)
+int		print_jobs(t_shell* shell, int *options)
 {
 	t_jobs_lst		*jobs;
 
@@ -75,21 +75,63 @@ int		print_jobs(t_shell *shell, int *options)
 			jobs->state, "Okay");//reconstruct_command(jobs->command));
 		jobs = jobs->next;
 	}
+	free(options);
+	return (0);
+}
+
+int		print_job(t_shell *shell, int *options, char **args, int i)
+{
+	t_jobs_lst		*jobs;
+
+	jobs = shell->jobs->lst;
+	while (jobs)
+	{
+		if (args[i] && jobs->job_number == ft_atoi(args[i]))
+		{
+			if (options[1] == 1)
+				ft_printf("%d\n", jobs->pid);
+			else if (options[0] == 1)
+				ft_printf("[%d] %c %d %s %s\n", jobs->job_number, jobs->current,
+				jobs->pid, jobs->state, "Okay");//reconstruct_command(jobs->command));
+			else
+				ft_printf("[%d] %c %s %s\n", jobs->job_number, jobs->current,
+				jobs->state, "Okay");//reconstruct_command(jobs->command));
+			i++;
+		}
+		jobs = jobs->next;
+	}
+	free(options);
 	return (0);
 }
 
 int		b_jobs(t_cmd *cmd, t_shell *shell)
 {
 	t_options	*opts;
+	int			i;
+	int			j;
 
-	(void)shell;
 	if (shell->jobs)
 	{
 		opts = opt_parse(cmd, "lp", "jobs");
+		i = 1;
+		while (cmd->args[i] && cmd->args[i][0] == '-')
+			i++;
+		j = i;
+		while (cmd->args[j++])
+		{
+			if (ft_atoi(cmd->args[j]) >= (int)shell->jobs->index || !ft_atoi(cmd->args[j]))
+			{
+				ft_dprintf(2, "bash: jobs: %s: no such job", cmd->args[j]);
+				return (1);
+			}
+			j++;
+		}
 		if (opts->ret != 0)
 			(opts->ret == -1 ? ft_putendl_fd("Usage: jobs [-l|-p] [job_id...]"
 			, 2) : 0);
-		else
+		else if (cmd->args[i])
+			print_job(shell, handle_options(opts), cmd->args, i);
+		else if (!cmd->args[i])
 			print_jobs(shell, handle_options(opts));
 		opt_free(opts);
 	}
