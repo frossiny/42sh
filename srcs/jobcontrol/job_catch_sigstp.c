ab9a1434b2afcec4e1d74a4026d3b39a37dbc48a
Luca BENARD <lubenard@student.42.fr>
2019-12-03 18:07:16 +0100

    Added comment about why is it not working

--- srcs/jobcontrol/job_catch_sigstp.c
+++ srcs/jobcontrol/job_catch_sigstp.c
@@ -6,12 +6,19 @@
 /*   By: frossiny <frossiny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 14:36:45 by frossiny          #+#    #+#             */
-/*   Updated: 2019/12/02 11:10:26 by frossiny         ###   ########.fr       */
+/*   Updated: 2019/12/03 17:59:29 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include "jobcontrol.h"
+# include <errno.h>
+
+/*
+** Setpgid fails because it returns Pemission denied (EACESS)
+** Why ? Because we already executed execve.
+** See man of setpgid
+*/
 
 void	job_catch_sigstp(int signal)
 {
@@ -20,8 +27,10 @@ void	job_catch_sigstp(int signal)
 	(void)signal;
 	if (g_child && g_shell.current_cmd)
 	{
-		kill(g_child, 17);
-		setpgid(g_child, 0);
+		kill(-g_child, 17);
+		ft_printf("g_child = %d\n", g_child);
+		if (setpgid(g_child, 0) < 0)
+			perror("setpgid failed");
 		job = job_new(g_shell.current_cmd, g_child);
 		job->state = "Stopped";
 		g_shell.current_cmd = NULL;
