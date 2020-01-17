e3d2d6d912f34c5fe7c18713a42411368cd63cc4
Valentin <saltel.valentin@gmail.com>
2020-01-17 15:02:29 +0100

    maj ctrl l

--- srcs/termcaps/t_clear.c
+++ srcs/termcaps/t_clear.c
@@ -6,7 +6,7 @@
 /*   By: vsaltel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 14:13:12 by vsaltel           #+#    #+#             */
-/*   Updated: 2020/01/16 14:50:30 by vsaltel          ###   ########.fr       */
+/*   Updated: 2020/01/17 13:34:59 by vsaltel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@
 
 void		termcaps_clear(char **str, t_cursor_pos *pos, t_shell *shell)
 {
+	(void)str;
+	(void)shell;
 	tputs(tgetstr("cl", NULL), 1, ft_putchar);
 	pos->y_min = 0;
-	reprint(*str, pos, g_pos.x_rel, 0);
 }
