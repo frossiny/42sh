92aeaaa0998d0aba04ce1ea635a2e3da4a44b929
Luca BENARD <lubenard@student.42.fr>
2020-01-24 09:41:05 +0100

    Added basic prompt expansions

--- srcs/expansion/prompt.c
+++ srcs/expansion/prompt.c
@@ -6,24 +6,83 @@
 /*   By: lubenard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 15:41:43 by lubenard          #+#    #+#             */
-/*   Updated: 2020/01/22 18:00:08 by lubenard         ###   ########.fr       */
+/*   Updated: 2020/01/24 09:39:23 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <shell.h>
 
+char	*stradd(char *s1, char s2)
+{
+	char	*dst;
+
+	if (!s1 || !s2 || !(dst = ft_strnew(ft_strlen(s1) + 1)))
+		return (NULL);
+	ft_strcpy(dst, s1);
+	dst[ft_strlen(s1) + 1] = s2;
+	return (dst);
+
+}
+
 void	handle_options_prompt(char *prompt)
 {
+	int		i;
+	char	*expanded_prompt;
+	char	hostname[4096];
 
-	(void)prompt;
-	//if (ft_strchr(prompt, "\\u"))
-		
+	i = 0;
+	expanded_prompt = "";
+	while (prompt[i])
+	{
+		if (prompt[i] == '\\' && prompt[i + 1] == 'u')
+		{
+			expanded_prompt = ft_strjoin(expanded_prompt, var_get(g_shell.vars,
+			"USER")->value);
+			//ft_printf("expanded prompt is |%s|\n", expanded_prompt);
+			i += 2;
+		}
+		else if (prompt[i] == '\\' && prompt[i + 1] == 'j')
+		{
+			expanded_prompt = ft_strjoin(expanded_prompt,
+			ft_itoa((int)g_shell.jobs.len));
+			//ft_printf("expanded prompt is |%s|\n", expanded_prompt);
+			i += 2;
+		}
+		else if (prompt[i] == '\\' && prompt[i + 1] == 'v')
+		{
+			//ft_printf("expanded_prompt in v = %s\n", expanded_prompt);
+			expanded_prompt = ft_strjoin(expanded_prompt, "1.0");
+			//ft_printf("expanded prompt is |%s|\n", expanded_prompt);
+			i += 2;
+		}
+		else if (prompt[i] == '\\' && prompt[i + 1] == 's')
+		{
+			expanded_prompt = ft_strjoin(expanded_prompt, "42sh");
+			//ft_printf("expanded prompt is |%s|\n", expanded_prompt);
+			i += 2;
+		}
+		else if (prompt[i] == '\\' && prompt[i + 1] == 'H')
+		{
+			gethostname(hostname, 4096);
+			expanded_prompt = ft_strjoin(expanded_prompt,
+			hostname);
+			//ft_printf("expanded prompt is |%s|\n", expanded_prompt);
+			i += 2;
+		}
+		else
+			expanded_prompt = stradd(expanded_prompt, prompt[i++]);
+	}
+	//ft_printf("expanded prompt is |%s|\n", expanded_prompt);
+	g_shell.ps1 = expanded_prompt;
 }
 
 void	prompt_expansions(void)
 {
-	if (var_get(g_shell.vars, "PS1"))
-		g_shell.ps1 = "lubenard - path \033[1;%dm$> \033[0m";
+	t_var	*ps1;
+
+	ps1 = var_get(g_shell.vars, "PS1");
+	if (ps1)
+		handle_options_prompt(ps1->value);
 	else
 		g_shell.ps1 = "\033[1;%dm$> \033[0m";
 }
--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: frossiny <frossiny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 14:41:10 by frossiny          #+#    #+#             */
-/*   Updated: 2020/01/22 17:50:49 by lubenard         ###   ########.fr       */
+/*   Updated: 2020/01/24 09:33:46 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	prompt(void)
 {
 	if (!isatty(0))
 		return ;
+	//prompt_expansions(); to call each time, but only once
 	if (g_ignore_signals == 0)
 		ft_printf(g_shell.ps1, g_return ? 31 : 32);
 	else if (g_ignore_signals == 1)
@@ -27,7 +28,7 @@ void	prompt(void)
 int		prompt_len(void)
 {
 	if (g_ignore_signals == 0)
-		return (ft_strlen("lubenard - path $> "));
+		return (ft_strlen(g_shell.ps1));
 	else if (g_ignore_signals == 1)
 		return (ft_strlen("> "));
 	return (0);
